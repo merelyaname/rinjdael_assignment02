63fb586a87d96d8bb75ec26286578ec3d88bd7ce
Anika Siddiqui <mayesha22jun@gmail.com>
2025-04-19 21:22:40 +0100

    added core functions to repo files

--- rijndael.h
+++ rijndael.h
@@ -1,6 +1,5 @@
 /*
- * TODO: Add your name and student number here, along with
- *       a brief description of this code.
+ * Submitted by: Anika Siddiqui Mayesha (D24125187).
  */
 
 #ifndef RIJNDAEL_H
@@ -14,7 +13,23 @@
  * entry point to the library for programmes hoping to use it to
  * encrypt or decrypt data)
  */
-unsigned char *aes_encrypt_block(unsigned char *plaintext, unsigned char *key);
-unsigned char *aes_decrypt_block(unsigned char *ciphertext, unsigned char *key);
 
-#endif
+// Core AES transformation functions
+void sub_bytes(unsigned char *state);
+void shift_rows(unsigned char *state);
+void mix_columns(unsigned char *state);
+void add_round_key(unsigned char *state, const unsigned char *round_key);
+
+// Inverse functions for decryption
+void inv_sub_bytes(unsigned char *state);
+void inv_shift_rows(unsigned char *state);
+void inv_mix_columns(unsigned char *state);
+
+// Key expansion
+unsigned char *expand_key(const unsigned char *key);
+
+// Main encryption and decryption functions
+unsigned char *aes_encrypt_block(const unsigned char *plaintext, const unsigned char *key);
+unsigned char *aes_decrypt_block(const unsigned char *ciphertext, const unsigned char *key);
+
+#endif /* RIJNDAEL_H */
